ca9936578d0e9005ee43342a704c1e85b34dc330
Trần Nhất Quang <trannhatquang2405@gmail.com>
2022-09-11 13:36:13 +0700

    fix

--- src/Components/Button/ChangeLanguageButton.jsx
+++ src/Components/Button/ChangeLanguageButton.jsx
@@ -3,29 +3,29 @@ import vi from "Assets/Images/vi_icon.png";
 import en from "Assets/Images/en_icon.png";
 
 const ChangeLanguageButton = (props) => {
-  const { i18n } = props;
+    const { i18n } = props;
 
-  const changeLanguage = () => {
-    if (i18n.language === "en") {
-      i18n.changeLanguage("vn");
-    } else {
-      i18n.changeLanguage("en");
-    }
-  };
+    const changeLanguage = () => {
+        if (i18n.language === "en") {
+            i18n.changeLanguage("vn");
+        } else {
+            i18n.changeLanguage("en");
+        }
+    };
 
-  return (
-    <span
-      onClick={changeLanguage}
-      style={{ height: 20, width: 30 }}
-    >
-      <img
-        alt={i18n.language === "en" ? "Tiếng Việt" : "English"}
-        src={i18n.language === "en" ? vi : en}
-        className="d-inline-block"
-        style={{ height: "100%", width: "100%" }}
-      />
-    </span>
-  );
+    return (
+        <div
+            onClick={changeLanguage}
+            style={{ height: "100%", width: 30, alignSelf: "center" }}
+        >
+            <img
+                alt={i18n.language === "en" ? "Tiếng Việt" : "English"}
+                src={i18n.language === "en" ? vi : en}
+                className="d-inline-block"
+                style={{ height: "100%", width: "100%" }}
+            />
+        </div>
+    );
 };
 
 export default withTranslation("translations")(ChangeLanguageButton);
--- src/Layout/Common/Header/Header.jsx
+++ src/Layout/Common/Header/Header.jsx
@@ -10,99 +10,99 @@ import { Row, Col } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 
 const Header = (props) => {
-  const navigate = useNavigate();
-  const { t } = useTranslation();
+    const navigate = useNavigate();
+    const { t } = useTranslation();
 
-  const onLogout = () => {
-    navigate("/SignIn");
-  };
+    const onLogout = () => {
+        navigate("/SignIn");
+    };
 
-  return (
-    <Row className="sticky-nav">
-      <Col className="bg-app-dark">
-        <Navbar expand="lg" variant="dark" className="Header__layout">
-          <Navbar.Brand
-            href="#home"
-            className="d-flex flex-row align-items-center"
-          >
-            <Logo isDark />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <NavDropdown
-                title={t("Jobs")}
-                id="basic-nav-dropdown"
-                menuVariant="dark"
-              >
-                <NavDropdown.Item href="#action">
-                  {t("Applied Jobs")}
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action">
-                  {t("Saved Jobs")}
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action">
-                  {t("Matched Jobs")}
-                </NavDropdown.Item>
-              </NavDropdown>
-              <NavDropdown
-                title={t("Profile & CV")}
-                id="basic-nav-dropdown"
-                menuVariant="dark"
-              >
-                <NavDropdown.Item href="#action">
-                  {t("CV Manage")}
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action">
-                  {t("CV Template")}
-                </NavDropdown.Item>
-              </NavDropdown>
-              <NavDropdown
-                title={t("Companies")}
-                id="basic-nav-dropdown"
-                menuVariant="dark"
-              >
-                <NavDropdown.Item href="#action">
-                  {t("Company List")}
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action">
-                  {t("Top Company")}
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav className="justify-content-end align-items-center">
-              <NavDropdown
-                title={
-                  <div className="d-flex flex-row align-items-center">
-                    <img
-                      alt=""
-                      src={user_img}
-                      width="40"
-                      height="40"
-                      className="d-inline-block rounded-circle"
-                    />
-                    <p className="mb-0 ms-2 Header__layout-text">
-                      {t("Your Profile")}
-                    </p>
-                  </div>
-                }
-                id="basic-nav-dropdown"
-                menuVariant="dark"
-              >
-                <NavDropdown.Item href="#action">
-                  {t("Edit Your Infomation")}
-                </NavDropdown.Item>
-                <NavDropdown.Item onClick={onLogout}>
-                  {t("Logout")}
-                </NavDropdown.Item>
-              </NavDropdown>
-              <ChangeLanguageButton />
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </Col>
-    </Row>
-  );
+    return (
+        <Row className="sticky-nav">
+            <Col className="bg-app-dark">
+                <Navbar expand="lg" variant="dark" className="Header__layout">
+                    <Navbar.Brand
+                        href="#home"
+                        className="d-flex flex-row align-items-center"
+                    >
+                        <Logo isDark />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <NavDropdown
+                                title={t("Jobs")}
+                                id="basic-nav-dropdown"
+                                menuVariant="dark"
+                            >
+                                <NavDropdown.Item href="#action">
+                                    {t("Applied Jobs")}
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action">
+                                    {t("Saved Jobs")}
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action">
+                                    {t("Matched Jobs")}
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown
+                                title={t("Profile & CV")}
+                                id="basic-nav-dropdown"
+                                menuVariant="dark"
+                            >
+                                <NavDropdown.Item href="#action">
+                                    {t("CV Manage")}
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action">
+                                    {t("CV Template")}
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown
+                                title={t("Companies")}
+                                id="basic-nav-dropdown"
+                                menuVariant="dark"
+                            >
+                                <NavDropdown.Item href="#action">
+                                    {t("Company List")}
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action">
+                                    {t("Top Company")}
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                        <Nav className="justify-content-end align-items-center">
+                            <NavDropdown
+                                title={
+                                    <div className="d-flex flex-row align-items-center">
+                                        <img
+                                            alt=""
+                                            src={user_img}
+                                            width="40"
+                                            height="40"
+                                            className="d-inline-block rounded-circle"
+                                        />
+                                        <p className="mb-0 ms-2 Header__layout-text">
+                                            {t("Your Profile")}
+                                        </p>
+                                    </div>
+                                }
+                                id="basic-nav-dropdown"
+                                menuVariant="dark"
+                            >
+                                <NavDropdown.Item href="#action">
+                                    {t("Edit Your Infomation")}
+                                </NavDropdown.Item>
+                                <NavDropdown.Item onClick={onLogout}>
+                                    {t("Logout")}
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <ChangeLanguageButton />
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </Col>
+        </Row>
+    );
 };
 
 export default Header;
