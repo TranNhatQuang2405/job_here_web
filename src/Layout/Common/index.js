5e06cf44f789901645f8cdeb609df29f5e4560a6
Lai Quang Phat <77201894+quangphatt@users.noreply.github.com>
2022-09-11 22:58:59 +0700

    JobItem

--- src/Layout/Common/index.js
+++ src/Layout/Common/index.js
@@ -1,3 +1,5 @@
 export { default as Header } from "./Header/Header";
 export { default as Footer } from "./Footer/Footer";
 export { default as Logo } from "./Logo/Logo";
+export { default as JobItem } from "./Jobs/JobItem/JobItem.jsx";
+export { default as JobList } from "./Jobs/JobList/JobList.jsx";
