db1fd00354b9be7ad860be276a073cef268d8cae
Lai Quang Phat <77201894+quangphatt@users.noreply.github.com>
2022-10-01 20:47:39 +0700

    Add API SignUp

--- src/Config/Api/ConfigURL.js
+++ src/Config/Api/ConfigURL.js
@@ -1,2 +1,4 @@
-export const signInURL = "/auth/login";
 export const getSessionURL = "/user/getSession";
+export const signInURL = "/auth/login";
+export const signUpURL = "/auth/register";
+export const authCodeURL = "/auth/authenticateCode";
--- src/Layout/Authentication/AuthCodePage/AuthCodePage.jsx
+++ src/Layout/Authentication/AuthCodePage/AuthCodePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import {
   Col,
   Row,
@@ -13,33 +13,60 @@ import {
 } from "react-bootstrap";
 import "./AuthCodePage.css";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { changeCurrentPage } from "Config/Redux/Slice/CurrentPageSlice";
 import { useTranslation } from "react-i18next";
+import { authBusiness } from "Business";
+import { WarningModal } from "Components/Modal";
+import { ValidateAuthCode } from "Config/Validate";
 
 const AuthCodePage = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const { t } = useTranslation();
   const [code, setCode] = useState("");
   const [alert, setAlert] = useState(false);
+  const modalRef = useRef();
 
   const onChangeCode = (e) => {
     setCode(e.target.value);
   };
 
-  const onAuthCode = (e) => {
+  const onAuthCode = async (e) => {
     e.preventDefault();
+    if (!ValidateAuthCode(code)) {
+      modalRef.current.setMessage("Invalid Email!");
+      modalRef.current.onToggleModal();
+      // setCode("")
+    } else {
+      let authCode = await authBusiness.AuthCode(parseInt(code));
+      if (authCode.data.httpCode === 200) {
+        dispatch(changeCurrentPage(1));
+        navigate("/SignIn");
+      } else {
+        modalRef.current.setMessage(
+          authCode.data.message || "Some thing went wrong! Please try again!"
+        );
+        modalRef.current.onToggleModal();
+        // setCode("")
+      }
+    }
   };
 
   const onResend = () => {};
 
   return (
     <div className="AuthCode">
+      <WarningModal ref={modalRef} />
       <div className="AuthCode__account-pages pt-5">
         <Row className="justify-content-center">
           <Col lg={4} xs={11}>
             <div className="text-center">
               <h4 className="AuthCode__title">{t("Authentication Code")}</h4>
               <p className="text-muted mb-1">
-                {t("Final step to treate your account.")}
+                {t(
+                  "Final step to treate your account. An Authentication Code has been sent to your email!"
+                )}
               </p>
             </div>
             <Card className="AuthCode__card mt-2">
